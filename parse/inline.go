9bc444b714f7c7c11f1af6e2fa1f9ee6e0e2f922
henrylee2cn <henrylee2cn@gmail.com>
2018-11-21 03:33:30 +0800

    chore: change fatalloop
    Change-Id: I04c6f75e2303301ac1f4c4b146afed05080d38ef

--- parse/inline.go
+++ parse/inline.go
@@ -99,7 +99,7 @@ func (f *FileSet) propInline() {
 }
 
 const fatalloop = `detected infinite recursion in inlining loop!
-Please file a bug at github.com/tinylib/msgp/issues!
+Please file a bug at github.com/henrylee2cn/msgp/issues!
 Thanks!
 `
 
